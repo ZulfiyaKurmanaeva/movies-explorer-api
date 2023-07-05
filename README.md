e1059782fbcc168328746d92ed86dece4dcf6554
Zulfiya Kurmanaeva <kurzul13@gmail.com>
2023-07-05 22:23:54 +0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # movies-explorer-api
+
+Бэкенд часть дипломной работы Movies Explorer.
+
+Адрес репозитория (backend): https://zulfiyakurmanaeva.github.io/movies-explorer-api/
+
+IP 51.250.20.219
+
+Backend  https://api.diplom.nomoreparties.sbs/
+
+Frontend (to be)  https://diplomov.nomoreparties.sbs/
---
+++ models/movie.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const isURL = require('validator/lib/isURL');
+
+const { invalidURL } = require('../utils/errorMessages');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => isURL(v),
+      message: invalidURL,
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => isURL(v),
+      message: invalidURL,
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => isURL(v),
+      message: invalidURL,
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
